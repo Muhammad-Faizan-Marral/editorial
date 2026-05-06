92965fab36d82105afbf3b8d5fa316b5e2f75d85
Faizan <marral7654321@gmail.com>
2026-05-06 16:42:20 +0500

    solving merging issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Header from './components/Header'
-import Portfolios from './components/Portfolios'
-import HeroSection from './components/HeroSection'
+import React from "react";
+import Header from "./components/Header";
+import Portfolios from "./components/Portfolios";
+import HeroSection from "./components/Herosection";
 
 
 const App = () => {
   return (
-    <div className='bg-white h-screen'>
-      <Header/>
-      <HeroSection/>
-       <Portfolios/>
+    <div className="bg-white h-screen">
+      <Header />
+      <HeroSection />
+      <Portfolios />
+    </div>
+  );
+};
 
-
-export default App
+export default App;
