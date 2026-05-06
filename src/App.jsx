7f5de380c77330218f9918d0b70ba5e488ef9ffc
Faizan <marral7654321@gmail.com>
2026-05-07 00:10:12 +0500

    Rearranging components

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,10 @@ const App = () => {
       <Header />
       <HeroSection />
       <Portfolios />
-      <TableSection/>
-      <FooterSection/>
       <ApprochSection/>
+      <TableSection/>
       <InquiryForm/>
+      <FooterSection/>
      </div>
   );
 };
